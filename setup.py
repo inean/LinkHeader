e0550aa39f76eb186be41914afb484d135d0257c
Michael Burrows <mjb@asplake.co.uk>
2010-01-18 17:55:03 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='LinkHeader',
+      version='0.1',
+      description='The parsing and formatting of Link Headers',
+      author='Michael Burrows',
+      author_email='mjb@asplake.co.uk   ',
+      url='http://bitbucket.org/asplake/link_header',
+      py_modules=['link_header']
+      classifiers=["Development Status :: TBC",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Programming Language :: Python",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Topic :: Software Development :: Libraries :: Python Modules"])
+
