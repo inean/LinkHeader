366c2e4ba9a5b5d57c684bfe48d226984a061375
Carlos Martín <cmartin@liberalia.net>
2012-04-16 18:16:20 +0200

    Rearrange file components and encode as utf-8/unix

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='LinkHeader',
-      version='0.2.2',
-      description='Parse and format link headers according to RFC 5988 "Web Linking"',
-      author='Michael Burrows',
-      author_email='mjb@asplake.co.uk',
-      url='http://bitbucket.org/asplake/link_header',
-      py_modules=['link_header'],
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Programming Language :: Python :: 2",
-                   "Programming Language :: Python :: 3",
-                   "Topic :: Internet :: WWW/HTTP",
-                   "Topic :: Software Development :: Libraries :: Python Modules"])
+setup(name="hal-json",
+      version="0.1",
+      description="Parse and encode links according to RFC 5988 or HAL specs",
+      author="Michael Burrows, Carlos Martín",
+      author_email="inean.es@gmail.com",
+      url="https://inean@github.com/inean/LinkHeader.git",
+      packages=find_packages(),
+      license="BSD",
+      keywords="RFC5988, HAL, json",
+      zip_safe=True,
+      long_description="""
+      A simple module to allow developers to format
+      or encode links according to RFC 5988 or trying to follow HAL
+      specifications (http://stateless.co/hal_specification.html)
+      """,
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Software Development :: Libraries :: Python Modules"
+      ]
+  )
